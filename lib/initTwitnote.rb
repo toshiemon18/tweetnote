777b1de4c171c6ee03391bac5aa43ae6b485076a
toshiemon18 <st12318@gmail.com>
2014-08-01 14:34:52 +0900

    プロセスが生きてるか確認する機能を追加

--- lib/initTwitnote.rb
+++ lib/initTwitnote.rb
@@ -15,13 +15,14 @@ class InitTwitNote
 
 	attr_accessor :track_word
 	attr_accessor :exit_command
+	attr_accessor :heartbeat_command
 	attr_accessor :feed_back
 
 	def initialize(key_word)
 		@twitclient = Tweetlib::Client.new(TWITTER)
 		@me = @twitclient.fetch_account_info
 		@token = EVERNOTE[2]
-		client = EvernoteOAuth::Client.new(
+		@client = EvernoteOAuth::Client.new(
 				token: @token,
 				consumer_key: EVERNOTE[0],
 				consumer_secret: EVERNOTE[1],
@@ -29,11 +30,18 @@ class InitTwitNote
 			)
 		@track_word = "#"+ "#{key_word}"
 		@exit_command = "--quit"
+		@heartbeat_command = "Yo"
 		@feed_back = FEED_BACK
 		begin
-			@note_store = client.note_store
+			@note_store = @client.note_store
 		rescue => e
 			puts e
+		end
+
+		begin
+			@user_store = @client.user_store
+		rescue  => e
+			
 		end
 	end
 end
